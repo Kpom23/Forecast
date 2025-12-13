ef05a844bb37d8e6b2830855b8146d7e8ee30b11
Kpom23 <143014073+Kpom23@users.noreply.github.com>
2025-12-13 17:56:12 -0600

    Injected more weather data

--- index.js
+++ index.js
@@ -2,10 +2,36 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description")
+  let humidityElement = document.querySelector("#humidity")
+  let windSpeedElement = document.querySelector("#wind-speed")
+  let timeElement = document.querySelector("#time")
+  let dateElement = new Date(response.data.time * 1000);
  
-
+console.log(response.data.condition.description);
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate (dateElement);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date){
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thrusday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+
 }
 
 function searchCity(city) {
